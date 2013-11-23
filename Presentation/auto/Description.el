40413cc2f175e3df0f9016e299562a64a79a69b2
Abraham Xiao <q289196573@Gmail.com>
2013-11-23 16:17:00 +0400

    Let the hunt begin. So much to do, with so little time. So what's your plan? I plan to stay here for a Ph.D. Because of the CARE-FREE environment, I can pay FULL attention to my research. And this in return does good to me, and Masdar Institute.
    Reviewed-by: Abraham Xiao <q289196573@Gmail.com>

---
+++ Presentation/auto/Description.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "Description"
+ (lambda ()
+    (TeX-run-style-hooks
+     "amsfonts"
+     "amsmath"
+     "graphicx"
+     "booktabs"
+     "latex2e"
+     "art10"
+     "article"
+     "")))
+
